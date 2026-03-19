82579deab9759c5c494185599eed0b49ae111436
Gema <Gema@DESKTOP-0JAC2PL>
2026-03-19 19:52:23 +0100

    Renombrar parametros de las funciones hipotenusa y catetoA

--- src/tarea4/Pruebas.java
+++ src/tarea4/Pruebas.java
@@ -57,12 +57,12 @@ public class Pruebas {
         return Math.pow(num, 2.0);
     }
 
-    public static double hipotenusa(double c1, double catetoB) {
-        return Math.sqrt(alCuadrado(c1) + alCuadrado(catetoB));
+    public static double hipotenusa(double cateto1, double c2) {
+        return Math.sqrt(alCuadrado(cateto1) + alCuadrado(c2));
     }
 
-    public static double catetoA(double hipotenusa, double catetoB) {
-        return Math.sqrt(alCuadrado(hipotenusa) - alCuadrado(catetoB));
+    public static double catetoA(double hipotenusa, double c) {
+        return Math.sqrt(alCuadrado(hipotenusa) - alCuadrado(c));
     }
 
     public static double mphAKmh(double mph) {
